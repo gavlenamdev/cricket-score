5a9587c4279c26c2743bbbb9ac4f13fb627880c7
gnaam <gavlenamdevprabha@gmail.com>
2019-06-17 13:13:29 +0530

    get score of specified 2 teams

--- cricket_score.py
+++ cricket_score.py
@@ -8,18 +8,20 @@ def set2str(output):
         final += s + "\n"
     return final
     
-def get_score():
+def get_score(team1, team2):
 	# returns top 2 scores
     res = requests.get("https://www.cricbuzz.com/cricket-match/live-scores")
     soup = BeautifulSoup(res.text,"html.parser")
     body = soup.find("body")
     text = body.get_text()
     scores = []
-    for obj in re.findall("[A-Z]{2,3}\s\d{1,3}[\/\-]\d{1,2}\s\(\d{1,2}\.\d Ovs\)",text):
-        scores.append(obj)
-        break
+    for obj in re.findall("[A-Z]{2,3}\s\d{1,3}[\/\-]{0,1}\d{1,2}\s\(\d{1,2}\.\d Ovs\)",text):
+    	if team1 in obj or team2 in obj:
+        	scores.append(obj)
+        # break
     for obj in re.findall("[A-Z]{2,3}\s\d{1,3}[\/\-][a-z]{3}\s[a-z]{3}\s\(\d{1,2}\.\d Ovs\)",text):
-        scores.append(obj)
-        break
+        if team1 in obj or team2 in obj:
+        	scores.append(obj)
+        # break
     output = set2str(scores)
     return output
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from cricket_score import get_score
 from notification import notifier
 import time
 
-refresh = 30
+refresh = 60
 while True:
-    score = get_score()
+    team1 = 'IND'
+    team2 = 'PAK'
+    score = get_score(team1, team2)
     print(score)
     notifier(score)
     time.sleep(refresh)
